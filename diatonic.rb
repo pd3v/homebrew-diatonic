9eec0a6e2d3ad2196bf2b16c1346f736e1152a00
pd3v <paulo.develop@gmail.com>
2022-11-10 16:04:29 +0000

    testing

--- diatonic.rb
+++ diatonic.rb
@@ -1,11 +1,21 @@
 class Diatonic < Formula
   desc "Diatonic transforms for music making."
-  homepage ""
+  homepage "https://github.com/pd3v/diatonic"
   url "https://github.com/pd3v/diatonic/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "7251d1b63cbf8f6edb9cbf68ee6ee3939621cee6803c1db757771d42d9958c2c"
   license "GPL-3.0"
 
+  depends_on "cmake" => :build
+
   def install
-    # bin.install "diatonic_demo"
+    bin.install "diatonic_demo"
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+  
+  test do
+    system "false"
   end
 end
+
